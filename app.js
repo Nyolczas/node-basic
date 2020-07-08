de38a39ce801dbe0ce63297e1772bd4bfb0be3fc
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2020-07-08 20:42:22 +0200

    express

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const http = require('http');
-const { sum, sub } = require('./helpers');
+// const http = require('http');
+// const { sum, sub } = require('./helpers');
 
-const server = http.createServer((req, res) => {
-    res.end('hello from node js');
-});
+// const server = http.createServer((req, res) => {
+//     res.end('pina from node js');
+// });
 
-server.listen(3000);
+// server.listen(3000);
 
-console.log(sum(259, 111), sub(259, 111));
+// console.log(sum(259, 111), sub(259, 111));
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("express pina");
+})
+
+app.listen(3000);
